b2268cf138491b9c037814e12ac77b9868d38b00
Myronenko <55333290+SergeyMyronenko@users.noreply.github.com>
2024-03-13 13:14:43 +0200

    Initial commit

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,28 @@
+import { FaPhoneAlt, FaUser } from "react-icons/fa";
+import css from "./Contact.module.css";
+
+export const Contact = ({ contacts: { name, number, id }, onDelete }) => {
+  const formatNumber = (inputNumber) => {
+    const pattern = /(\d{3})(\d{2})(\d{2})/;
+    const formatedNumber = inputNumber.replace(pattern, "$1-$2-$3");
+    return formatedNumber;
+  };
+
+  return (
+    <div className={css.item}>
+      <div className={css.paragraph}>
+        <p>
+          <FaUser className={css.icon} />
+          {name}
+        </p>
+        <p>
+          <FaPhoneAlt className={css.user} />
+          {formatNumber(number)}
+        </p>
+      </div>
+      <button className={css.button} type="button" onClick={() => onDelete(id)}>
+        Delete
+      </button>
+    </div>
+  );
+};
